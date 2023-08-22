d090766959994d816247a930eb6738026761cc8a
PhucTD <trandaiphuc.1501@gmail.com>
2023-08-22 21:55:03 +0700

    first commit

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+app_dir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig:
+    DEBUG = True
+    POSTGRES_URL="udacitylearning-new.postgres.database.azure.com:5432"  #TODO: Update value
+    POSTGRES_USER="udacitylearning@udacitylearning" #TODO: Update value
+    POSTGRES_PW="Abcde12345-+"   #TODO: Update value
+    POSTGRES_DB="udacitylearning"   #TODO: Update value
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
+    CONFERENCE_ID = 1
+    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SERVICE_BUS_CONNECTION_STRING ='' #TODO: Update value
+    SERVICE_BUS_QUEUE_NAME ='notificationqueue'
+    ADMIN_EMAIL_ADDRESS: 'admin@gmail.com'
+    SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
